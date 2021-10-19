fd6417b7f34d43976ad48d04018f10db9ae72d56
Jean SCULLY <jescully@e2r10p20.42.fr>
2021-10-19 14:38:17 +0200

    adapted split is now norme-adjacent, still need to adapt it so it deals with all whitespaces instead of just one sep char

--- tests/jesccully_test/ft_better_split.c
+++ tests/jesccully_test/ft_better_split.c
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_better_split.c                                  :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jescully <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/19 10:59:38 by jescully          #+#    #+#             */
-/*   Updated: 2021/10/19 13:23:13 by jescully         ###   ########.fr       */
-/*                                                                            */
+/*																			  */
+/*														  :::	   ::::::::   */
+/*	 ft_better_split.c									:+:		 :+:	:+:   */
+/*													  +:+ +:+		  +:+	  */
+/*	 By: jescully <marvin@42.fr>					+#+  +:+	   +#+		  */
+/*												  +#+#+#+#+#+	+#+			  */
+/*	 Created: 2021/10/19 10:59:38 by jescully		   #+#	  #+#			  */
+/*	 Updated: 2021/10/19 14:25:08 by jescully		  ###	########.fr		  */
+/*																			  */
 /* ************************************************************************** */
 
 #include "../../libs/libft/libft.h"
@@ -21,38 +21,33 @@ static int	ft_ischar(char o, char c)
 
 int isquote(char c)
 {
-    if (c == '\'')
-        return (1);
-    else if (c == '"')
-        return (2);
-    else 
-        return (0);
+	if (c == '\'')
+		return (1);
+	else if (c == '"')
+		return (2);
+	else 
+		return (0);
 }
 
 static int	ft_countthewords(char const *s, char c)
 {
 	int		i;
 	int		cw;
-    int     quotes;
+	int		quotes;
 
-	i = 0;
+	i = -1;
 	cw = 0;
-    quotes = 0;
-	while (s[i])
+	quotes = 0;
+	while (s[++i])
 	{
-        if (isquote(s[i]))
-        {
-            quotes = isquote(s[i]);
-            while (quotes)
-            {
-                i++;
-                if (isquote(s[i]) == quotes)
-                    quotes = 0;
-            }
-        }
+		quotes = isquote(s[i]);
+		while (quotes)
+		{
+			if (isquote(s[++i]) == quotes)
+				quotes = 0;
+		}
 		if (s[i] == c && s[i + 1] != c)
 			cw++;
-		i++;
 	}
 	if (i == 0)
 		return (0);
@@ -77,9 +72,39 @@ static int	ftwtw(int len, char **strs, int row, const char *s)
 	return (row);
 }
 
+int fill_word(char const *s, int lead, char c)
+{
+	while (!ft_ischar(s[lead], c) && s[lead])
+	{
+		int quote = 0;
+		{
+			lead++;
+			if (isquote(s[lead]))
+				quote = isquote(s[lead]);
+			while (quote)
+			{
+				lead++;
+				if (isquote(s[lead]) == quote)
+					quote = 0;
+			}
+		}
+	}
+	return (lead);
+}
+
+int go_through_quote(char const *s, int lead, int *quote)
+{
+	while (*quote)
+	{
+		lead++;
+		if (isquote(s[lead]) == *quote)
+			*quote = 0;
+	}
+	return (lead);
+}
+
 char	**ft_better_split(char const *s, char c)
 {
-	int		wc;
 	char	**strs;
 	int		lead;
 	int		follow;
@@ -89,42 +114,21 @@ char	**ft_better_split(char const *s, char c)
 	row = 0;
 	lead = 0;
 	quote = 0;
-	wc = ft_countthewords(s, c);
-	strs = (char **)ft_calloc((wc + 1), sizeof(char *));
+	strs = (char **)ft_calloc((ft_countthewords(s, c) + 1), sizeof(char *));
 	if (!strs)
 		return (NULL);
-    while (row < wc)
-    {
-        while (ft_ischar(s[lead], c) && !quote)
-            lead++;
-        if (isquote(s[lead]))
-        {
-            quote = isquote(s[lead]);
-        }
-          
-        follow = lead;
-        while (quote)
-        {
-            lead++;
-            if (isquote(s[lead]) == quote)
-                quote = 0;
-        }
-        while (!ft_ischar(s[lead], c) && s[lead])
-        {
-            while (quote)
-            {
-                lead++;
-                if (isquote(s[lead]) == quote)
-                    quote = 0;
-            }
-            lead++;
-            if (isquote(s[lead]))
-                quote = isquote(s[lead]);
-        }
-        strs[row] = (char *)malloc(sizeof(char) * ((lead - follow + 1)));
-        if (!strs)
-            return (NULL);
-        row = ftwtw(lead - follow, strs, row, (s + follow));
-    }
-    return (strs);
+	while (row < ft_countthewords(s, c))
+	{
+		while (ft_ischar(s[lead], c) && !quote)
+			lead++;
+		quote = isquote(s[lead]);
+		follow = lead;
+		lead = go_through_quote(s, lead, &quote);
+		lead = fill_word(s, lead, c);
+		strs[row] = (char *)malloc(sizeof(char) * ((lead - follow + 1)));
+		if (!strs)
+			return (NULL);
+		row = ftwtw(lead - follow, strs, row, (s + follow));
+	}
+	return (strs);
 }
