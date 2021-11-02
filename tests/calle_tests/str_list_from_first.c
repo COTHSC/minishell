fbbff7686f1a0c4dc3dcfbde582e38b3fcc64444
COTHSC <thierryscully.colomban@gmail.com>
2021-11-02 19:48:00 +0100

    it works?

--- tests/calle_tests/str_list_from_first.c
+++ tests/calle_tests/str_list_from_first.c
@@ -6,12 +6,24 @@ int main(void )
 {
 	char **list;
 	char ***p_list;
+    int i = 0;
 
 	list = ft_split("xHello xWorld", ' ');
 	p_list = &list;
+    while (p_list[0][i])
+    {
+        printf("%s\n", &p_list[0][i][1]);
+        i++;
+    }
+    i = 0;
+    while (list[i])
+    {
+        printf("%s\n", &list[i][1]);
+        i++;
+    }
+
 	//print_str_list(list, NULL);
-	print_str_list(&(*p_list[1]), NULL);
-	free_str_list(list, strlen_list(list));
+//	print_str_list(&(*p_list[1]), NULL);
 	return (0);
 }
 
