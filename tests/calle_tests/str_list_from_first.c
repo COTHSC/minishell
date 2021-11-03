20b4f8048af92a4e8f1d7ecc15922b405a71eeab
cnstll <calle@student.42.fr>
2021-11-03 13:17:11 +0100

    POC on handling env

--- tests/calle_tests/str_list_from_first.c
+++ tests/calle_tests/str_list_from_first.c
@@ -2,6 +2,8 @@
 #include "../../libs/libft/libft.h"
 #include "../../libs/libft+/libftp.h"
 
+char **g_env;
+
 void	print_my_list(char **my_list)
 {
     int i = 0;
@@ -12,26 +14,78 @@ void	print_my_list(char **my_list)
     }
 }
 
+char **env_selector(int position_selector)
+{
+	char	**selection;
+	int		i;
+
+	selection = calloc_str_list(strlen_list(g_env));
+	i = 0;
+	while (g_env[i])
+	{
+		selection[i] = ft_strdup(g_env[i] + position_selector);
+		i++;
+	}
+	return (selection);
+}
+
+void	alter_env(int pos_to_alter, char *replace)
+{
+	free(g_env[pos_to_alter]);
+	g_env[pos_to_alter] = ft_strdup(replace);
+}
+
 int main(void )
 {
-	char **list;
-	char ***p_list;
-    int i = 0;
+	{
+		//print env without flag
+		char **carved_env;
 
-	list = ft_split("xHello xWorld", ' ');
-	p_list = &list;
-    while (p_list[0][i])
-    {
-        printf("%s\n", &p_list[0][i][1]);
-        i++;
-    }
-    i = 0;
-    while (list[i])
-    {
-        printf("%s\n", &list[i][1]);
-        i++;
-    }
-	print_my_list((&list[0][1]));
+		g_env = ft_split("xHello xWorld", ' ');
+		carved_env = env_selector(1);
+		print_my_list(carved_env);
+		free_str_list(carved_env, strlen_list(carved_env));
+		free_str_list(g_env, strlen_list(g_env));
+	}
+	{
+		//alter an element in environnement base on matching var 
+		char **carved_env;
+
+		g_env = ft_split("xHello xWorld", ' ');
+		carved_env = env_selector(1);
+		if (ft_strncmp(carved_env[0], "Hello", 5) == 0)
+			alter_env(0, "xYATTA");
+		print_my_list(g_env);
+		free_str_list(carved_env, strlen_list(carved_env));
+		free_str_list(g_env, strlen_list(g_env));
+	}
+	//char ***p_list;
+//	p_list = &list;
+   // int i = 0;
+   // i = 0;
+   // while (list[i])
+   // {
+   //     printf("%s\n", (*(list + i) + 1));
+   //     i++;
+   // }
+//    i = 0;
+//    while ((*p_list + i))
+//    {
+//        printf("%s\n", *(*p_list + i) + 1);
+//        i++;
+//    }
+    //while (p_list[0][i])
+    //{
+    //    printf("%s\n", &p_list[0][i][1]);
+    //    i++;
+    //}
+    //i = 0;
+    //while (list[i])
+    //{
+    //    printf("%s\n", &list[i][1]);
+    //    i++;
+    //}
+	//print_my_list((&p_list[0][1]));
 
 	//print_str_list(list, NULL);
 //	print_str_list(&(*p_list[1]), NULL);
