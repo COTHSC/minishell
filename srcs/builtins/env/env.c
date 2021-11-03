45974168da83394f10147a07f5e4a8c191377e8a
cnstll <calle@student.42.fr>
2021-11-03 16:43:34 +0100

    Plug g_env in builtin ft_env

--- srcs/builtins/env/env.c
+++ srcs/builtins/env/env.c
@@ -6,22 +6,22 @@
 /*   By: calle <calle@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 12:24:08 by calle             #+#    #+#             */
-/*   Updated: 2021/11/02 18:56:53 by calle            ###   ########.fr       */
+/*   Updated: 2021/11/03 14:33:45 by calle            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../builtins.h"
 
-void	print_exported_var_with_values(char **var_list)
+void	print_exported_var_with_values( void )
 {
 	int	i;
 
 	i = 0;
-	while (var_list[i])
+	while (g_env[i])
 	{
-		if (var_is_exported(var_list[i]) && var_has_value(var_list[i]))
+		if (var_is_exported(g_env[i]) && var_has_value(g_env[i]))
 		{
-			ft_putstr_fd(var_list[i], 1);
+			ft_putstr_fd(&g_env[i][1], 1);
 			ft_putchar_fd('\n', 1);
 		}
 		i++;
@@ -38,7 +38,7 @@ int	ft_env(int argc, char **argv)
 //		perror(NULL);
 //		return (EXIT_FAILURE);
 //	}
-	print_exported_var_with_values(g_env);
-//	print_str_if_value_is_defined(argv + 1);
+	print_exported_var_with_values();
+	print_str_list(argv + 1, NULL);
 	return (EXIT_SUCCESS);
 }
