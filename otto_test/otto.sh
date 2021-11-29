57e1aeb1429c32bd9c3af7cc13cd00a049640812
cnstll <calle@student.42.fr>
2021-11-29 12:34:26 +0100

    Otto maj: Otto is smarter on analyzing diff file in errors tests: only first lines of error outputs are compared

--- otto_test/otto.sh
+++ otto_test/otto.sh
@@ -213,7 +213,10 @@ check_error()
 {
 	BASH_ERR=$(sed -n 's/bash\: line *[0-9]\: //p' $1)
 	MINISHELL_ERR=$(sed -n 's/minishell\: //p' $2)
+    BASH_ERR=$(echo "$BASH_ERR" | head -1)
+    #BASH_ERR=$(sed -n 1p $BASH_ERR)
 	CHECK_DIFF_ERR=$(diff <(echo "$BASH_ERR") <(echo "$MINISHELL_ERR"))
+    echo "DIFF: $CHECK_DIFF_ERR"
 	BASH_STATUS=$(cat $3)
 	MINISHELL_STATUS=$(cat $4)
 	CHECK_DIFF_STATUS=$(diff <(echo "$BASH_STATUS") <(echo "$MINISHELL_STATUS"))
