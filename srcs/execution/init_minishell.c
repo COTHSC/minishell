c1981a901a374c1e2b70e2003b063b83f6151a5f
cnstll <calle@student.42.fr>
2021-12-01 18:53:49 +0100

    solving displya issue with termios

--- srcs/execution/init_minishell.c
+++ srcs/execution/init_minishell.c
@@ -2,14 +2,14 @@
 
 int init_minishell(void)
 {
-    int             check_ret;
-    static struct	sigaction sa;
+	int             check_ret;
+	static struct	sigaction sa;
 
 	check_ret = terminal_settings();
-    if (check_ret != EXIT_SUCCESS)
-    {
-        exit(EXIT_FAILURE);
-    }
+	if (check_ret != EXIT_SUCCESS)
+	{
+		exit(EXIT_FAILURE);
+	}
 	signal_handler_settings(&sa);
-    return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
