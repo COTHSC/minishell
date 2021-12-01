ab89ddd04d650ece38af8ccc6bbe99991e5e5d69
cnstll <calle@student.42.fr>
2021-12-01 17:40:07 +0100

    Adding term setter for heredocs

--- srcs/execution/term_settings.c
+++ srcs/execution/term_settings.c
@@ -1,21 +1,36 @@
 # include "../../includes/minishell.h"
 
-static void modify_terminal_attributes(struct termios *newtio)
-{
-	newtio->c_lflag &= ~(ICANON);
-	newtio->c_lflag |= ICANON; // Set terminal in canonical
-	newtio->c_lflag &= ~(ISIG);
-	newtio->c_lflag |= ISIG; // Set signal handling options
-	newtio->c_lflag &= ~(ECHOCTL); // Unset ctrl + <char> printing
-	newtio->c_cc[VQUIT] = 4; // Rewire SIGQUIT to ctrl + D
-}
-
-static int set_terminal_attributes(struct termios *newtio)
+static int set_parent_attributes(struct termios *parent_tio)
 {
     int ret;
 
-	modify_terminal_attributes(newtio);
-	ret = tcsetattr(STDIN_FILENO, TCSANOW, newtio); //update current terminal attributes
+	parent_tio->c_lflag &= ~(ICANON);
+	parent_tio->c_lflag |= ICANON; // Set terminal in canonical
+	parent_tio->c_lflag &= ~(ISIG);
+	parent_tio->c_lflag |= ISIG; // Set signal handling options
+	parent_tio->c_lflag &= ~(ECHOCTL); // Unset ctrl + <char> printing
+    parent_tio->c_cc[VQUIT] = 4; // Rewire SIGQUIT to ctrl + D
+	ret = tcsetattr(STDIN_FILENO, TCSANOW, parent_tio); //update current terminal attributes
+	if (ret < 0)
+	{
+		perror ("error in tcsetattr");
+		return (EXIT_FAILURE);
+	}
+    else
+        return (EXIT_SUCCESS);
+}
+
+static int set_hd_attributes(struct termios *hd_tio)
+{
+    int ret;
+
+	hd_tio->c_lflag &= ~(ICANON);
+	hd_tio->c_lflag |= ICANON; // Set terminal in canonical
+	hd_tio->c_lflag &= ~(ISIG);
+	hd_tio->c_lflag |= ISIG; // Set signal handling options
+	hd_tio->c_lflag &= ~(ECHOCTL); // Unset ctrl + <char> printing
+    hd_tio->c_cc[VQUIT] = 0; // Rewire SIGQUIT to ctrl + D
+	ret = tcsetattr(STDIN_FILENO, TCSANOW, hd_tio); //update current terminal attributes
 	if (ret < 0)
 	{
 		perror ("error in tcsetattr");
@@ -33,6 +48,13 @@ int	terminal_settings(void)
         return (EXIT_SUCCESS);
     else if (!setch_og_tio(0))
         return (EXIT_FAILURE);
-    ret = set_terminal_attributes(setch_parent_tio(0));
+    if (!setch_parent_tio(0)) 
+        return (EXIT_FAILURE);
+    if (!setch_hd_tio(0)) 
+        return (EXIT_FAILURE);
+    ret = set_parent_attributes(setch_parent_tio(1));
+    if (ret == EXIT_FAILURE)
+        return (ret);
+    ret = set_hd_attributes(setch_hd_tio(1));
 	return (ret);
 }
