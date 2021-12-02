9c97b947980c56055739a2671b594b03e1a2f461
cnstll <calle@student.42.fr>
2021-12-02 14:45:06 +0100

    Norme done for buitlins files

--- srcs/builtins/builtins.h
+++ srcs/builtins/builtins.h
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: calle <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 14:42:33 by calle             #+#    #+#             */
+/*   Updated: 2021/12/02 14:42:36 by calle            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef BUILTINS_H
 # define BUILTINS_H
-#include "../../libs/libft/libft.h"
-#include "../../libs/libft+/libftp.h"
-#include "../../includes/minishell.h"
+# include "../../libs/libft/libft.h"
+# include "../../libs/libft+/libftp.h"
+# include "../../includes/minishell.h"
 # include <errno.h>
 # include <dirent.h>
 # include <limits.h>
@@ -11,15 +23,14 @@
 # include <sys/types.h>
 # include <unistd.h>
 # define OFFSET_ES 1
-
 /*
 ** ------------------ BUILTINS FUNCTIONS ------------------
 */
-int 	ft_echo(int argc, char **argv);
-int 	ft_cd(int argc, char **argv);
-int 	ft_pwd(int argc, char **argv);
-int 	ft_export(int argc, char **argv);
-int		ft_unset(int argc, char **argv);
-int		ft_env(int argc, char **argv);
-int		ft_exit(int argc, char **argv);
+int	ft_echo(int argc, char **argv);
+int	ft_cd(int argc, char **argv);
+int	ft_pwd(int argc, char **argv);
+int	ft_export(int argc, char **argv);
+int	ft_unset(int argc, char **argv);
+int	ft_env(int argc, char **argv);
+int	ft_exit(int argc, char **argv);
 #endif
--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: calle <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 10:47:24 by calle             #+#    #+#             */
+/*   Updated: 2021/12/02 10:49:43 by calle            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "builtins.h"
 
 char	**delete_var(char **clean_env, char *var_to_del)
@@ -32,7 +44,7 @@ int	is_a_valid_name(char *var)
 	int	i;
 
 	i = 0;
-	while (var[i])	
+	while (var[i])
 	{
 		if (!ft_isanywordchar(var[i]))
 			return (0);
@@ -41,55 +53,55 @@ int	is_a_valid_name(char *var)
 	return (1);
 }
 
-int delete_from_env(char *var_to_unset)
+int	delete_from_env(char *var_to_unset)
 {
 	char	**tmp;
 	char	**name_value_pair;
 	char	**clean_env;
 
-    name_value_pair = split_to_name_value_pair(var_to_unset);
-    clean_env = env_selector(1, NULL);
-    if (match_var_name(clean_env, name_value_pair[0]))
-    {
-        tmp = delete_var(clean_env, name_value_pair[0]);
-        if (!tmp)
-            return (EXIT_FAILURE);
-        free_str_list(g_env, strlen_list(g_env));
-        g_env = tmp;
-    }
-    free_str_list(clean_env, strlen_list(clean_env));
-    free_str_list(name_value_pair, strlen_list(name_value_pair));
-    return (EXIT_SUCCESS);
+	name_value_pair = split_to_name_value_pair(var_to_unset);
+	clean_env = env_selector(1, NULL);
+	if (match_var_name(clean_env, name_value_pair[0]))
+	{
+		tmp = delete_var(clean_env, name_value_pair[0]);
+		if (!tmp)
+			return (EXIT_FAILURE);
+		free_str_list(g_env, strlen_list(g_env));
+		g_env = tmp;
+	}
+	free_str_list(clean_env, strlen_list(clean_env));
+	free_str_list(name_value_pair, strlen_list(name_value_pair));
+	return (EXIT_SUCCESS);
 }
 
 int	check_and_delete_from_env(char **vars_to_unset)
 {
 	int		i;
-    int     catch_error;
-    int     catch_ret;
+	int		catch_error;
+	int		catch_ret;
 
 	i = 0;
-    catch_error = 0;
+	catch_error = 0;
 	while (vars_to_unset[++i])
 	{
 		if (!is_a_valid_name(vars_to_unset[i]))
 		{
 			perror_not_a_valid_identifier(vars_to_unset[i], "unset");
-            catch_error = 1;
-			continue;
+			catch_error = 1;
+			continue ;
 		}
-        catch_ret = delete_from_env(vars_to_unset[i]);
-        if (catch_ret == EXIT_FAILURE)
-            return (EXIT_FAILURE);
+		catch_ret = delete_from_env(vars_to_unset[i]);
+		if (catch_ret == EXIT_FAILURE)
+			return (EXIT_FAILURE);
 	}
-    return (catch_error);
+	return (catch_error);
 }
 
 int	ft_unset(int argc, char **argv)
 {
 	if (argc == 1)
 		return (EXIT_SUCCESS);
-	else if (is_option(argv[1]) && argv[1][1])	
+	else if (is_option(argv[1]) && argv[1][1])
 	{
 		perror_invalid_option("unset", argv[1], NULL);
 		print_usage_unset();
