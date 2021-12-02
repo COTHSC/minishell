0970788092345284c2fc9d16d08f4a57e2d95499
cnstll <calle@student.42.fr>
2021-12-02 13:28:40 +0100

    Fixing termios paramters init

--- srcs/execution/term_settings.c
+++ srcs/execution/term_settings.c
@@ -20,30 +20,19 @@ static int set_parent_attributes(struct termios *parent_tio)
         return (EXIT_SUCCESS);
 }
 
-static int set_hd_attributes(struct termios *hd_tio)
+static int load_hd_attributes(struct termios *hd_tio)
 {
-    int ret;
-
 	hd_tio->c_lflag &= ~(ICANON);
 	hd_tio->c_lflag |= ICANON; // Set terminal in canonical
 	hd_tio->c_lflag &= ~(ISIG);
 	hd_tio->c_lflag |= ISIG; // Set signal handling options
 	hd_tio->c_lflag &= ~(ECHOCTL); // Unset ctrl + <char> printing
     hd_tio->c_cc[VQUIT] = 0; // Rewire SIGQUIT to ctrl + D
-	ret = tcsetattr(STDIN_FILENO, TCSANOW, hd_tio); //update current terminal attributes
-	if (ret < 0)
-	{
-		perror ("error in tcsetattr");
-		return (EXIT_FAILURE);
-	}
-    else
-        return (EXIT_SUCCESS);
+    return (EXIT_SUCCESS);
 }
 
 int	terminal_settings(void)
 {
-	int	ret;
-
 	if (!isatty(STDIN_FILENO))
 		return (EXIT_SUCCESS);
     if (!setch_og_tio(0))
@@ -52,9 +41,6 @@ int	terminal_settings(void)
         return (EXIT_FAILURE);
     if (!setch_hd_tio(0)) 
         return (EXIT_FAILURE);
-    ret = set_parent_attributes(setch_parent_tio(1));
-    if (ret == EXIT_FAILURE)
-        return (ret);
-    ret = set_hd_attributes(setch_hd_tio(1));
-	return (ret);
+    load_hd_attributes(setch_hd_tio(1));
+    return (set_parent_attributes(setch_parent_tio(1)));
 }
