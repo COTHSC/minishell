dfdb1790129fe0d7a68cf163d516387629d64cfd
cnstll <calle@student.42.fr>
2021-12-02 15:56:56 +0100

    Refacto env modifier

--- srcs/env_handling/check_and_alter_env.c
+++ srcs/env_handling/check_and_alter_env.c
@@ -1,38 +1,45 @@
 #include "../../includes/minishell.h"
 
-static void	perror_and_set_error(char *arg_error, char *builtin, int *error, int error_value)
+int	check_env_wrapper(char *arg, char *flag, char **clean_env)
 {
-	perror_not_a_valid_identifier(arg_error, builtin);
-	*error = error_value;
+	int		iie;
+	char	**name_value_pair;
+	int		ret_value;
+
+	ret_value = 0;
+	name_value_pair = split_to_name_value_pair(arg);
+	iie = index_matching_var_name(clean_env, name_value_pair[0]);
+	if (ft_strlen(name_value_pair[0]) == 1 && arg[0] == '_')
+		;
+	else if (ft_strncmp(flag, "x", 1) == 0 && !has_valid_var_name(arg))
+	{
+		perror_not_a_valid_identifier(arg, "export");
+		ret_value = 1;
+	}
+	else if (iie == -1 || (flag[0] == 'd' && !var_is_exported(g_env[iie])))
+		ret_value = alter_env_var(clean_env, name_value_pair[0], arg, flag);
+	else if (flag[0] == 'x' && !var_has_value(arg))
+		change_flag(&g_env[iie], flag[0]);
+	else
+		ret_value = alter_env_var(clean_env, name_value_pair[0], arg, "x");
+	free_str_list(name_value_pair, strlen_list(name_value_pair));
+	return (0);
 }
 
 int	check_and_alter_env(char **args, char *flag)
 {
 	int		i;
-	int		idx_var_to_alter;
 	char	**clean_env;
-	char	**name_value_pair;
 	int		ret_value;
 
 	clean_env = env_selector(1, NULL);
 	ret_value = 0;
-	i = -1;
-	while (args[++i])
+	i = 0;
+	while (args[i])
 	{
-		name_value_pair = split_to_name_value_pair(args[i]);
-		idx_var_to_alter = index_matching_var_name(clean_env, name_value_pair[0]);
-        if (ft_strlen(name_value_pair[0]) == 1 && args[i][0] == '_')
-            continue ;
-		if (ft_strncmp(flag, "x", 1) == 0 && !has_valid_var_name(args[i]))
-			perror_and_set_error(args[i], "export", &ret_value, 1);
-		else if (idx_var_to_alter == -1
-			|| (flag[0] == 'd' && !var_is_exported(g_env[idx_var_to_alter])))
-			alter_env_var(clean_env, name_value_pair[0], args[i], flag);
-		else if (flag[0] == 'x' && !var_has_value(args[i]))
-			change_flag(&g_env[idx_var_to_alter], flag[0]);
-		else
-			alter_env_var(clean_env, name_value_pair[0], args[i], "x");
-		free_str_list(name_value_pair, strlen_list(name_value_pair));
+		if (check_env_wrapper(args[i], flag, clean_env) != 0)
+			ret_value = 1;
+		i++;
 	}
 	free_str_list(clean_env, strlen_list(clean_env));
 	return (ret_value);
